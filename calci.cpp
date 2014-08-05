4a7e8e43fbafec64d74ae5deae8d37e83d7edffb
supriyabade <supriya.bade@cumminscollege.in>
2014-08-05 08:58:11 +0530

    Update calci.cpp

--- calci.cpp
+++ calci.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
 using namespace std;
+class sum{
+  
+public:
+int add()
+  {
+    int a=10;
+    int b=20;
+    int c=a+b;
+    return c;
+    
+  }
+  };
 void main()
+
 {
+  
 cout<<"Good Morning";
+sum s;
+int d=s.add();
+cout<<"c="<<d;
+
 }
